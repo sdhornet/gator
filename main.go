dc285aebb104162ff305aea50fa856b50f424884
Nate <nate@DS-0220392.localdomain>
2026-08-03 19:05:15 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
 	"github.com/sdhornet/gator/internal/config"
 )
 
-const username = "nate"
+type state struct {
+	cfg *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	handlers map[string]func(*state, command) error
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("Missing username")
+	}
+	username := cmd.args[2]
+
+	if err := s.cfg.SetUser(username); err != nil {
+		return err
+	}
+
+	fmt.Printf("Username set to: %s\n", username)
+	return nil
+}
 
 func main() {
 	cfg, err := config.Read()
