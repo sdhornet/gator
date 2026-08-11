608a6cae1f2bc012c24f54b9b2dcec160260db10
Nate <nate@DS-0220392.localdomain>
2026-08-11 16:28:01 -0500

    Added in the middleware function, but it has not been integrated yet.

--- handler_feed.go
+++ handler_feed.go
@@ -29,6 +29,16 @@ type RSSItem struct {
 	PubDate     string `xml:"pubDate"`
 }
 
+func middlewareLoggedIn(handler func(s *state, cmd command, user database.User) error) func(*state, command) error {
+	return func(s *state, cmd command) error {
+		user, err := s.db.GetUserByName(context.Background(), s.cfg.CurrentUserName)
+		if err != nil {
+			return err
+		}
+		return handler(s, cmd, user)
+	}
+}
+
 func handlerAgg(s *state, cmd command) error {
 	feedURL := "https://www.wagslane.dev/index.xml"
 
